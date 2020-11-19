2d92848c96e75745040bfd5d162c088089c85696
Aleko <alex.kasongolenge@yahoo.com>
2020-11-19 01:24:15 -0800

    reducer now working

--- src/App.js
+++ src/App.js
@@ -12,6 +12,26 @@ import PokemonTable from './components/PokemonTable'
 import PokemonContext from './PokemonContext'
 
 /*
+** @emotion/styled is Case Sensitive, also pretty much the equivelant on vuejs's 
+** built in <style></style>
+*/
+const Title = styled.h1`
+  text-align: center; 
+`;
+
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  grid-column-gap: 1rem;
+`;
+
+const PageContainer = styled.div`
+  margin: auto;
+  width: 800px;
+  paddingTop: 1rem;
+`;
+
+/*
   ** Reducer: a function that takes state and an action, the state is the current state of the store
   ** the action is an object that defines the mutation that is to be applied to state 
   ** and then a new state is to be returned
@@ -40,71 +60,52 @@ const pokemonReducer = (state, action) => {
 }
 
 /*
-** @emotion/styled is Case Sensitive, also pretty much the equivelant on vuejs's 
-** built in <style></style>
-*/
-const Title = styled.h1`
-  text-align: center; 
-`;
-
-const TwoColumnLayout = styled.div`
-  display: grid;
-  grid-template-columns: 70% 30%;
-  grid-column-gap: 1rem;
-`;
-
-const PageContainer = styled.div`
-  margin: auto;
-  width: 800px;
-  paddingTop: 1rem;
-`;
-
-/*
 ** Function based App component
 */
 function App() {
   /*
   ** We use react hook to get state. eg. selectedPokemonSet sets the state for selectedPokemon  
   */
-  const [filter, filterSet] = React.useState("");
-  const [pokemon, pokemonSet] = React.useState([]);
-  const [selectedPokemon, selectedPokemonSet] = React.useState(null);
+  // const [filter, filterSet] = React.useState("");
+  // const [pokemon, pokemonSet] = React.useState([]);
+  // const [selectedPokemon, selectedPokemonSet] = React.useState(null);
   const [state, dispatch] = React.useReducer(pokemonReducer, {
     pokemon: [],
     filter: '',
     selectedPokemon: null
   })
 
-  if (!state.pokemon) {
-    return <div>Loading data</div>
-  }
-
   /*
   ** useEffect() runs afunction in reaction to a change. Functions are placed in the array
   */
   React.useEffect(() => {
     fetch('http://localhost:3000/starting-react/pokemon.json')
-      .then(resp => resp.json())
-      .then(data => dispatch({
-        type: 'SET_POKEMON',
-        payload: data
-      })
+      .then((resp) => resp.json())
+      .then((payload) =>
+        dispatch({
+          type: 'SET_POKEMON',
+          payload: payload
+        })
       );
     //if the array is empty, this function gets run once when the page is loaded/mounted,
     // like vuejs's mounted
-  }, [])
+  }, []);
+
+  if (!state.pokemon) {
+    return <div>Loading data</div>
+  }
 
   return (
     // we wrap everything inside the provider, which is going to provide the data 
     // to any component in the tree from this level down
     <PokemonContext.Provider
       value={{
-        filter,
-        filterSet,
-        pokemon,
-        pokemonSet,
-        selectedPokemon,
-        selectedPokemonSet,
+        // filter,
+        // filterSet,
+        // pokemon,
+        // pokemonSet,
+        // selectedPokemon,
+        // selectedPokemonSet,
         state,
         dispatch,
       }}
